064a420913f11a15b7c3f2ca2d30257caa3515f6
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2023-07-02 12:34:08 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    //circle
+	    int r = 6;
+	    double pi = 3.14;
+	    double S = pi * r * r;
+	    double C = 2 * pi * r;
+		System.out.println(" Perimetr " + C);
+		System.out.println(" Area " + S);
+	}
+}
